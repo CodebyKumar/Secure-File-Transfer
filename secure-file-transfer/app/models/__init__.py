8fdec6f1bcca771f1d80c94da42d0c4d17d8eab7
Kumarswami Kallimath <153998849+CodebyKumar@users.noreply.github.com>
2025-11-15 22:58:40 +0530

    Delete secure-file-transfer directory

--- secure-file-transfer/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .file import File
-from .access import Access
-
-__all__ = ["User", "File", "Access"]
--- secure-file-transfer/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from secure-file-transfer!")
-
-
-if __name__ == "__main__":
-    main()
